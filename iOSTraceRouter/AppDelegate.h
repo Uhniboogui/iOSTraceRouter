66e5662bd6f8991194b8b50cfd70d5cfb97d08a9
Songhyun <shpark006@gmail.com>
2016-01-03 10:22:23 +0900

    Initial Commit

---
+++ iOSTraceRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTraceRouter
+//
+//  Created by ParkSh on 2016. 1. 3..
+//  Copyright © 2016년 Songhyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSTraceRouter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSTraceRouter
+//
+//  Created by ParkSh on 2016. 1. 3..
+//  Copyright © 2016년 Songhyun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
